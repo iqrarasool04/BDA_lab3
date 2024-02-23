2c20cdf897c15b17a3646a0bea1067cbfa9eee1a
iqrarasool04 <iqrarasool45@gmail.com>
2024-02-23 15:50:31 +0500

    change

--- task_4.py
+++ task_4.py
@@ -15,7 +15,7 @@ frequent_itemsets = fpgrowth(df, min_support=0.2, use_colnames=True)
 
 #calculating length of itemsets
 frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x:len(x))
-filtered_itemsets = frequent_itemsets[(frequent_itemsets['length']>=2) & (frequent_itemsets['support']>=0.2)]
+filtered_itemsets = frequent_itemsets[(frequent_itemsets['length']>=1) & (frequent_itemsets['support']>=0.2)]
 
 #extracting association rules
 rules = association_rules(filtered_itemsets, metric="confidence", min_threshold=0.7, support_only=False)
